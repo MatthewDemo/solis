960e380921297aecc344c8c046981a895689614a
Matviy <kmilyj72@gmail.com>
2023-05-18 18:02:57 +0300

    deployment bugs fixed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -12,11 +12,19 @@ const Header = () => {
     <div className={headerClassName}>
       <img src={LogoImage} alt="" className="logo-image" />
       <ul className={navbarClassName}>
-        <li><a target="_blank" href="https://github.com/MatthewDemo?tab=repositories">Home</a></li>
+        <li>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/MatthewDemo?tab=repositories"
+          >
+            Home
+          </a>
+        </li>
         <li>About</li>
         <li>Products</li>
         <li>Use cases</li>
-        <li onClick={() => alert('Я хочу лише від 400$ на місяць')}>Pricing</li>
+        <li onClick={() => alert("Я хочу лише від 400$ на місяць")}>Pricing</li>
         <li>Resources</li>
       </ul>
     </div>
